aaa089fcabe6d6236fe3466450bd6c35ebb83262
yoshiky <y.nagatsuka0524@gmail.com>
2013-02-14 21:15:31 +0900

    first project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysns::Application.config.secret_token = 'f70abce568d014e9a99787921c624668a5dd808cfcff97effcad53a6cefe5dcbbf5d4ec470947b44077a0e84b8419470500bde5257646529ad7773a3fbe69425'
